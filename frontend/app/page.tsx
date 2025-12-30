3fb653bba126d30bde0c89fd9adbb0f88f0de480
Priyanka Neogi <157391369+PriyankaNeogi@users.noreply.github.com>
2025-12-30 16:13:53 +0530

    Add homepage page.tsx

---
+++ frontend/app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ChatUI from "@/components/ChatUI";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-4">
+      <ChatUI />
+    </main>
+  );
+}
